c2d15d082c484153772fa54b0772007bc4743a21
Jean Carlo <jean160599@gmail.com>
2026-02-24 00:43:51 -0400

    adicionando data anotacion no dto de criar contato

--- API/DTOs/Contatos/ContatoCriarDto.cs
+++ API/DTOs/Contatos/ContatoCriarDto.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
@@ -23,6 +24,8 @@ namespace API_AGENDA.DTOs
         public string? Email { get; set;}
         [MaxLength(50)]
         public string? Categoria { get; set;}
+
+        [DefaultValue(false)]
         public bool Favorito { get; set;}
 
     }
